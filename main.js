a0e79658b6f42be705e055e3b20dcb3a1cdf4894
DanneCarlson <113188007+DanneCarlson@users.noreply.github.com>
2022-09-27 20:47:31 +0200

    27_9_2

--- main.js
+++ main.js
@@ -25,15 +25,41 @@ Använd appendChild för att lägga till i ul
 
 Kolla "span"! (Andreas presentation)
 */
+//const nyUppgift = document.getElementById('nyUppgift');
 
-const nyUppgift = document.querySelector("#nyUppgift");
+
+
+// Hämtar in referens till listan
+const listan = document.querySelector("#list");
+
 const laggTillBtn = document.querySelector("#laggTillBtn");
-const newListElement = document.querySelector("#list");
+//const labelInput = document.querySelector("#labelInput");
 
+const returInfo = document.querySelector("#info");
 
 laggTillBtn.addEventListener("click", function(){
 
-    list.appendChild(nyUppgift);
+    let nyUppgift = document.querySelector("#nyUppgift");
+    let nySakAttGora = nyUppgift.value;                         // Tar in det som matats in i fältet
+    
+    if (nySakAttGora.length == 0){
+        returInfo.innerHTML = "Vänligen ange en uppgift, mothertrucker :)";
+        return;
+    } else {
+        let sakAttGora = document.createElement('li');
+        let sakAttGoraLabel = document.createElement('span');
+    
+        listan.appendChild(sakAttGora);                             // Lägger till en rad i listan
+        sakAttGoraLabel.innerText = nySakAttGora; 
+        sakAttGora.appendChild(sakAttGoraLabel);
+
+        //Tömmer inmatningsfältet
+        nyUppgift.value = "";
+        returInfo.innerHTML = "";
+    }
+
+    
+
   });
 
 
