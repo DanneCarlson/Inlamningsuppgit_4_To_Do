02fd7ca4d865184d9151a0cb97bc88e934af9d45
DanneCarlson <113188007+DanneCarlson@users.noreply.github.com>
2022-10-04 11:09:48 +0200

    10_4_2

--- main.js
+++ main.js
@@ -3,47 +3,50 @@
 // Hämtar in referens till listan
 const listan = document.querySelector("#list");
 
+// Hämtar in referens till knappen
 const laggTillBtn = document.querySelector("#laggTillBtn");
 
+// Här visas text om användaren inte angett något
 const returInfo = document.querySelector("#info");
 
+// Här visas antalet avklarade uppgifter
 const avklarat = document.querySelector("#avklarat");
 let avklaradeUppgifter = 0;
 
-// Kika på det där med array, både lägga till och ta bort
+// Array där inmatade uppgifter läggs till
 const todoArray = [];
+
+// Referens till det som användaren matar in i textfältet
+const nyUppgift = document.querySelector("#nyUppgift");
 
 
 laggTillBtn.addEventListener("click", function(){
 
-    // Behöver man ta in hela elementet först?
-    let nyUppgift = document.querySelector("#nyUppgift");
-    let nySakAttGora = nyUppgift.value;                         // Tar in det som matats in i fältet
+    // Lokal variabel för det som användaren matat in
+    let nySakAttGora = nyUppgift.value;                         
     
-    
-    
+    // Om användaren inte matat in något i textfältet
     if (nySakAttGora.length == 0){
-        returInfo.innerHTML = "Vänligen ange en uppgift, mothertrucker :)";
+        returInfo.innerHTML = "Vänligen ange en uppgift";
         return;
     } else {
-        let sakAttGora = document.createElement('li');
-        const sakAttGoraLabel = document.createElement('span');
-        todoArray.push(nySakAttGora);
+        let sakAttGora = document.createElement('li');              // Skapar plats i listan
+        let sakAttGoraLabel = document.createElement('span');       // Skapar ett span i listan
+        todoArray.push(nySakAttGora);                               // Lägg till uppgiften i arrayen
         listan.appendChild(sakAttGora);                             // Lägger till en rad i listan
-        sakAttGoraLabel.innerText = nySakAttGora; 
-        sakAttGora.appendChild(sakAttGoraLabel);
+        sakAttGoraLabel.innerText = nySakAttGora;                   // Visar den nya uppgiften
+        sakAttGora.appendChild(sakAttGoraLabel);                    // Lägger till uppgiften i listelementet
 
-        // Detta funkar också.
-        //sakAttGoraLabel.classList.add("completed");
-
+      
+        // När en klickar på span/uppgift i listan
         sakAttGoraLabel.addEventListener('click', function(){
-        if (sakAttGora.getAttribute('class') == 'completed') {
+        if (sakAttGora.getAttribute('class') == 'completed') {      // Om uppgiften redan har klassen utförd
         sakAttGora.setAttribute('class', '');
-        avklaradeUppgifter--;
+        avklaradeUppgifter--;                                       // Subtrahera från avklarade uppgifter
         }
         else {
-        sakAttGora.setAttribute('class', 'completed');
-        avklaradeUppgifter++;
+        sakAttGora.setAttribute('class', 'completed');              // Annars ge den klassen completed via css
+        avklaradeUppgifter++;                                       // Addera till avklarade uppgifter
 
         
         }
@@ -54,7 +57,7 @@ laggTillBtn.addEventListener("click", function(){
         //todoObject.status = "ej avklarad";
         todoArray.push(todoObject);*/
 
-        avklarat.innerHTML = `${avklaradeUppgifter} uppgift/er avklarad/e`;
+        avklarat.innerHTML = `${avklaradeUppgifter} uppgift/er avklarad/e`;     // Visar antalet avklarade uppgifter
 
         // Lägga till papperskorg:
         // Font Awesome?
