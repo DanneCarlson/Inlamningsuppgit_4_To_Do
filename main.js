b8e544d43427bb8fdfa011ddda753fd701cfc553
DanneCarlson <113188007+DanneCarlson@users.noreply.github.com>
2022-10-08 18:07:49 +0200

    10_8

--- main.js
+++ main.js
@@ -17,14 +17,16 @@ const todoArray = [];
 // Referens till det som användaren matar in i textfältet
 const nyUppgift = document.querySelector("#nyUppgift");
 
+
+
+let papperskorg = '<i class="fa-regular fa-trash-can"></i>';
+
+let img = '<img src="trash.png">';
+
+
 laggTillBtn.addEventListener("click", function () {
   // Lokal variabel för det som användaren matat in
   let nySakAttGora = nyUppgift.value;
-  let papperskorg = ["Ta bort"]; // Här vill jag ha en bild
-
-
-  /*let papperskorg = [];
-  papperskorg[0] = "trash.png";*/
 
   // Om användaren inte matat in något i textfältet
   if (nySakAttGora.length == 0) {
@@ -33,20 +35,13 @@ laggTillBtn.addEventListener("click", function () {
   } else {
     let sakAttGora = document.createElement("li"); // Skapar plats i listan
     let sakAttGoraLabel = document.createElement("span"); // Skapar ett span i listan
-
-    // Hur skapar jag ett tomrum mellan uppgiften och ta bort?
-    let tomrumLabel = document.createElement("span");
-    tomrumLabel.innerHTML = ":    :    :";
-
     let taBortLabel = document.createElement("span");
-    taBortLabel.innerHTML = papperskorg[0];
-
-    
+    taBortLabel.innerHTML = papperskorg;
 
     listan.appendChild(sakAttGora); // Lägger till element/rad i listan
     sakAttGoraLabel.innerText = nySakAttGora; // Sätter det som användaren matat in
-    sakAttGora.appendChild(sakAttGoraLabel); // Lägger till uppgiften i listelementet +++++ ett till span
-    sakAttGora.appendChild(tomrumLabel);
+    sakAttGora.appendChild(sakAttGoraLabel); // Lägger till uppgiften i listelementet 
+    //sakAttGora.appendChild(tomrumLabel);
     sakAttGora.appendChild(taBortLabel);
 
     todoArray.push(nySakAttGora); // Lägg till uppgiften i arrayen
@@ -62,19 +57,7 @@ laggTillBtn.addEventListener("click", function () {
         avklaradeUppgifter++; // Addera till avklarade uppgifter
       }
 
-      /*// Tillagt 29/9
-        const todoObject = {};
-        todoObject.todo = nyUppgift;
-        //todoObject.status = "ej avklarad";
-        todoArray.push(todoObject);*/
-
-     
-
       avklarat.innerHTML = `${avklaradeUppgifter} uppgift/er avklarad/e`; // Visar antalet avklarade uppgifter
-
-      // Lägga till papperskorg:
-      // Font Awesome?
-      // Emojis, kolla w3schools: &#1F5D1 (papperskorg)
     });
 
     taBortLabel.addEventListener("click", function () {
@@ -83,12 +66,10 @@ laggTillBtn.addEventListener("click", function () {
         avklaradeUppgifter--; // Subtrahera från avklarade uppgifter
       }
       sakAttGora.remove(sakAttGora);
+      
       avklarat.innerHTML = `${avklaradeUppgifter} uppgift/er avklarad/e`;
     });
 
-    //avklarat.innerHTML = `${avklaradeUppgifter} uppgift/er avklarad/e`; // Visar antalet avklarade uppgifter
-
-    
     //Tömmer inmatningsfältet och infofältet
     nyUppgift.value = "";
     returInfo.innerHTML = "";
